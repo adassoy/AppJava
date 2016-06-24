fbdef553f2e5ef305dbd61879b64f701d968170d
Curso <Curso@ForGrb15.ipartekaula.com>
2016-06-24 10:24:53 +0200

    initial import

---
+++ src/com/ipartek/formacion/AppJava/AppJava.java
@@ -0,0 +1,11 @@
+package com.ipartek.formacion.AppJava;
+
+public class AppJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Soy una App camuflada");
+
+	}
+
+}
