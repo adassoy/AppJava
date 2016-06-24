51f0d4420046fcd0cab6ed15fe9574e3c5d94bd2
Curso <Curso@ForGrb15.ipartekaula.com>
2016-06-24 10:29:20 +0200

    Add Syso "commit2"

--- src/com/ipartek/formacion/AppJava/AppJava.java
+++ src/com/ipartek/formacion/AppJava/AppJava.java
@@ -5,6 +5,7 @@ public class AppJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Soy una App camuflada");
+		System.out.println("Segundo Commit");
 
 	}
 
